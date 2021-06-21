fbb796ca7054f34369bca43d4808b822d0e8cea8
unknown <oumaymamesbahi2@gmail.com>
2021-06-21 12:43:34 +0200

    component

---
+++ src/App.js
@@ -0,0 +1,26 @@
+
+import './App.css';
+import téléchargement from './imgs/téléchargement.jpg'
+import './style.css';
+
+import Header from './component/header.js';
+import About from './component/about.js';
+import Project from './component/projects.js';
+import Contact from './component/contact.js';
+import Footer from './component/footer.js';
+
+
+
+
+  function App() {
+    return(
+    <>
+    < Header/>
+    <About/>
+    <Project/>
+    <Contact/>
+    <Footer/>
+    </>
+    )
+  }
+  export default App
